af8d91ca3b9be1a0e951d2051838cacbdb056099
Moizshah <moiz.shah624@gmail.com>
2018-07-19 00:50:46 +0500

    changes

---
+++ app/src/main/java/com/javacodegeeks/androidaccelerometerexample/Sample.java
@@ -0,0 +1,26 @@
+package com.javacodegeeks.androidaccelerometerexample;
+
+/**
+ * Created by QuellX on 5/24/2018.
+ */
+
+public class Sample {
+
+    int id;
+    float value;
+
+    public Sample(int id, float value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public float getValue() {
+        return value;
+    }
+
+}
